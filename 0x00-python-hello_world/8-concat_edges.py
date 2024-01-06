bc1be27c5ee151b8de07d99bfaf3312d3d0dbd1e
Grace Mtaje <gracemtaje5@gmail.com>
2024-01-06 09:02:50 -0800

    all files for python

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+my_str = "Python is an interpreted, interactive, object-oriented programming" \
+         " language that combines remarkable power with very clear syntax"
+
+my_str = my_str[:39] + my_str[67:107] + my_str[112:116] + my_str[:6]
+print(my_str)
+
+
