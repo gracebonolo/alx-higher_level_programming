041faf208843b26e388d8050b900a7a6e67a602e
Grace Mtaje <gracemtaje5@gmail.com>
2024-04-26 16:10:51 -0700

    c is fun

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,5 +5,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
-        header = response.info()
-        print(header['X-Request-Id'])
+        x_request_id = response.getheader('X-Request-Id')
+        print(x_request_id)
